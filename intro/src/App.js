13c06bc9c4e2ec6c698c8d5de7f87ca5d3baf46d
erayberatatalay <erayberat37@gmail.com>
2020-08-22 18:57:11 +0300

    reactstrap and bootsrap import

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navi from './Navi';
+import CategoryList from './CategoryList';
+import ProductList from './ProductList';
+import { Container, Row, Col } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs={3}>
+            <CategoryList />
+          </Col>
+          <Col xs={9}>
+            <ProductList />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ intro/src/Navi.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class componentName extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Navi Component</h2>
+            </div>
+        );
+    }
+}
