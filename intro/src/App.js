f2a8bedd4207c3f9c82477d831a6de7466b58437
erayberatatalay <erayberat37@gmail.com>
2020-08-22 19:18:26 +0300

    props

--- intro/src/App.js
+++ intro/src/App.js
@@ -5,6 +5,8 @@ import ProductList from './ProductList';
 import { Container, Row, Col } from 'reactstrap';
 
 function App() {
+  let productInfo={title:"Product List"}
+  let categoryInfo={title:"Category List"}
   return (
     <div className="App">
       <Container>
@@ -13,10 +15,10 @@ function App() {
         </Row>
         <Row>
           <Col xs={3}>
-            <CategoryList />
+            <CategoryList info={categoryInfo}/>
           </Col>
           <Col xs={9}>
-            <ProductList />
+            <ProductList info={productInfo}/>
           </Col>
         </Row>
       </Container>
