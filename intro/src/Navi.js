e9d02fa148adbc6cdf516f59fdf5ac7814248065
erayberatatalay <erayberat37@gmail.com>
2020-08-23 15:09:45 +0300

    Form operations - Submit

--- intro/src/Navi.js
+++ intro/src/Navi.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import {
-    Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink
-} from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import CartSummary from './CartSummary';
-
+import { Link } from 'react-router-dom';
 export default class Navi extends Component {
     constructor(props) {
         super(props);
@@ -26,12 +24,14 @@ export default class Navi extends Component {
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
                             <NavItem>
-                                <NavLink href="/components/">Components</NavLink>
+                                <NavLink >
+                                    <Link to="/form1/">Form Demo 1</Link>
+                                </NavLink>
                             </NavItem>
                             <NavItem>
                                 <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
                             </NavItem>
-                            <CartSummary removeFromCart={this.props.removeFromCart} cart={this.props.cart}/>
+                            <CartSummary removeFromCart={this.props.removeFromCart} cart={this.props.cart} />
                         </Nav>
                     </Collapse>
                 </Navbar>
